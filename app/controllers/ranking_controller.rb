c55af9570215a0c54cc2ddcb19fcc83dad2db6bd
travelistars <travelistars@gmail.com>
2016-01-19 12:07:57 +0000

    save

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,13 +1,34 @@
 class RankingController < ApplicationController
     
-    def create
-    end
+    # def create
+    #  @items = Item.find(params[:item_id])
     
-    def show
-        @ranking = Ranking.find(params[:id])
-        @ranking = Ranking.find(:all, :order => "Ranking Desc")
-        
+    # if params[:type] == 'have'
+    #     @ranking = @items.order("have_users.count desc")
+    #     render 'show'
+    # else
+    #     @ranking = @items.order("want_users.count desc")
+    #     render 'show'
+    # end
+
+    # end
+    
+    # def show
+    # end
+    
+    def have
+        # @rainking = Have.~~~
+        # 使うメソッドはgroup order limit count
+        # @ranking = [{アイテムのID ： count数},{アイテムのID ： count数},{アイテムのID ： count数},{アイテムのID ： count数}]
+        @ranking = Have.group(:item_id).order('count_item_id desc').limit(10).count('item_id')
+        item_ids = @ranking.keys
+        @items = Item.find(item_ids).sort_by{ |o| item_ids.index(o.id) }
     end
-    def destroy
+
+    def want
+        @ranking = Want.group(:item_id).order('count_item_id desc').limit(10).count('item_id')
+        item_ids = @ranking.keys
+        @items = Item.find(item_ids).sort_by{ |o| item_ids.index(o.id) }
     end
+
 end
